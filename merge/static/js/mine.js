ec8c5fd23c65726449910bc6787edbc352b3cf1d
thirumurugan <thiru123arul@outlook.com>
2021-04-26 02:28:07 +0530

    added for mobile use

--- merge/static/js/mine.js
+++ merge/static/js/mine.js
@@ -9,9 +9,6 @@ $(document).ready(function(){
     $("#type-frame div span").click(function(){
         $("#chng-span").text($(this).text());
     });
-    $("#dragndrop-ui").click(function(){
-        init();
-    });
 });
 $(function() { 
     $( "#dragndrop-ui" ).sortable({ 
@@ -29,27 +26,3 @@ function deleteEle(elem){
 $.ajaxSetup({
     headers: { "X-CSRFToken": '{{csrf_token}}' }
 });
-
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
